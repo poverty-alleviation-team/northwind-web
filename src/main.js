2eb0d3b2eb2783b9333e2037002a3a8ca762ae13
Karl.Zhang <karl_zhang0510@gmail.com>
2019-04-09 14:11:52 +0800

    添加vue-axios运行依赖
    main.js中配置全局使用axios

--- src/main.js
+++ src/main.js
@@ -3,15 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import './theme/index.css'
 import './assets/css/font-awesome.min.css'
 import './assets/css/style.css'
+
 import Config from './config/'
 
 //Vue.config.productionTip = false
 Vue.prototype.$Config = Config
+Vue.prototype.$axios = axios
 
 Vue.use(ElementUI);
+Vue.use(VueAxios,axios);
 
 new Vue({
   router,
